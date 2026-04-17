631042982b100bb1981b2f5f83fa51424668b7be
M Tahir Shabbir <tahirshabbir955@gmail.com>
2026-04-17 17:26:47 +0500

    made new updates on frontend (p 1,7,8) 17-Apr-2026

--- config/sasTenantId.ts
+++ config/sasTenantId.ts
@@ -1,7 +1,7 @@
 // export const SAAS_Tenant_ID = "28c71425-577a-43ad-aa5f-9494a90c7b2e";
 
-// export const SAAS_Tenant_ID = "cbb4331b-b4d9-498b-aa52-281d00330708";
+export const SAAS_Tenant_ID = "cbb4331b-b4d9-498b-aa52-281d00330708";
 
-export const SAAS_Tenant_ID = "bd0bf3b0-5cb7-42cb-8f6a-323fa2925b01";
+// export const SAAS_Tenant_ID = "bd0bf3b0-5cb7-42cb-8f6a-323fa2925b01";
 
 
